f60e3332c94de0947868642bcd031f55e1a62533
f-sara <jyujyusa.68@gmail.com>
2023-09-25 13:22:14 +0900

    postsテーブルを作成

--- database/migrations/2023_09_25_030244_create_posts_table.php
+++ database/migrations/2023_09_25_030244_create_posts_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name', 255);
+            $table->text('contents');
             $table->timestamps();
         });
     }
