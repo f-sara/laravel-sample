e843dc19bc6bc6217cd0622a172fa7c428344249
f-sara <jyujyusa.68@gmail.com>
2023-10-23 14:38:48 +0900

    feat: PostControllerを作成

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view("posts.post", compact("posts"));
+    }
 }
+
